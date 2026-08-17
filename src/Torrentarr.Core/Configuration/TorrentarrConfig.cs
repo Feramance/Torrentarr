1f3bacf404b060eec3b1bb292295572e1c9a5a86
Feramance <38938175+Feramance@users.noreply.github.com>
2026-08-17 20:45:59 +0000

    [skip ci] Bump version to v6.12.5

--- src/Torrentarr.Core/Configuration/TorrentarrConfig.cs
+++ src/Torrentarr.Core/Configuration/TorrentarrConfig.cs
@@ -19,7 +19,7 @@ public class TorrentarrConfig
 
 public class SettingsConfig
 {
-    public string ConfigVersion { get; set; } = "6.12.4";
+    public string ConfigVersion { get; set; } = "6.12.5";
     public string ConsoleLevel { get; set; } = "INFO";
     public bool Logging { get; set; } = true;
     public string CompletedDownloadFolder { get; set; } = "";
